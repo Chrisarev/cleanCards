188463a9a5be75774057f6568c0d1ed91b401d35
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-02-06 19:43:09 -0800

    Added image preview when user selects file for input type file. Added basic styling of cards in addCard component

--- src/AddCard.js
+++ src/AddCard.js
@@ -1,21 +1,41 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import Navbar from "./Navbar2";
 import styles from "./stylesheets/addCard.module.css"
+
 const AddCard = () => {
     const { deckID } = useParams();
+    const [firstImage, setFirstImage] = useState(''); 
+    const [secondImage, setSecondImage] = useState('')
+    
+    ///allows preview of image when user selects one(before upload)
+    const fileOnChange = (event) => {
+        const file = event.target.files[0];
+        if(file) {
+            if(event.target.id==='firstImageInput'){
+                setFirstImage(URL.createObjectURL(file));
+            }else if(event.target.id==='secondImageInput'){
+                setSecondImage(URL.createObjectURL(file));
+            }
+        }
+        console.log(event.target.id); 
+    };
 
-    return ( 
+    return (
         <>
         <div>{deckID}</div>
         <div className={styles.cardPanel}>
             <form action="">
             <div className={styles.card}>
                 <h1>FrontSide</h1>
-                <input type="file" name="image"></input>
+                <img src={firstImage} alt="" />
+                <input type="file" name="firstImage" onChange={fileOnChange} id="firstImageInput"></input>
             </div>
             <div className={styles.card}>
                 <h1>BackSide</h1>
+                <img src={secondImage} alt="" />
+                <input type="file" name="secondImage" onChange={fileOnChange} id="secondImageInput"></input>
+
             </div>
             </form>
         </div>
