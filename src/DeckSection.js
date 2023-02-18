6b4b09a63bc493074c21a126988bb4ca55521ba3
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-02-17 22:37:37 -0800

    Finished homepage cardSection svgs, Formatted deckSection

--- src/DeckSection.js
+++ src/DeckSection.js
@@ -3,8 +3,13 @@ import styles from './stylesheets/deckSection.module.css'
 const DeckSection = () => {
     return (  
         <div className={styles.panel}>
-            <h1 className={styles.deckHeader}>Create and save multiple decks for different topics!</h1>
-            
+            <div className={styles.textSection}>
+                <h1 className={styles.deckHeader}>Create and save multiple decks for different topics!</h1>
+                <h2 className={styles.deckSubHeader}>No matter what the subject, we've got you covered.</h2>
+            </div>
+            <div className={styles.imageSection}>
+                <img></img>
+            </div>
         </div>
     );
 }
